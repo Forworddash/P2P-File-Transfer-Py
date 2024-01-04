1fc8c8d93e70c24695d70d1e74e07624fd7c83f7
samuel <samuel.baker@rack-a-tiers.com>
2024-01-03 19:43:51 -0800

    - [FIX] dependency circle fix in server_ui

--- server.py
+++ server.py
@@ -1,21 +1,33 @@
 import socket
 
-s = socket.socket()
-host = socket.gethostname()
-port = 8080
-s.bind((host, port))
-s.listen(1)
-print(host)
-print('waiting for incoming connections...')
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
 
-conn, addr = s.accept()
-print(addr, "Has connected to the server")
+    def start(self):
+        self.server_socket.listen(1)
+        print(self.host)
+        print('waiting for incoming connections...')
 
+        conn, addr = self.server_socket.accept()
+        print(addr, "has connected to the server")
 
+        filename = input("Please enter the filename of the file: ")
+        self.send_file(conn, filename)
 
-filename = input(str("Please enter the filename of the file : "))
-file = open(filename, 'rb')
-filedata = file.read(1024)
-print(filedata)
-conn.send(filedata)
-print("Data has been transmitted successfully")
+    def send_file(self, conn, filename):
+        file = open(filename, 'rb')
+        filedata = file.read(1024)
+        print(filedata)
+        conn.send(filedata)
+        print("Data has been transmitted successfully")
+
+if __name__ == "__main__":
+    host = "127.0.0.1"
+    port = 8080
+
+    server = Server(host, port)
+    server.start()
--- server_ui.py
+++ server_ui.py
@@ -1,39 +1,23 @@
-import socket
-import threading
 import tkinter as tk
-from server_ui import ServerUI
+from server import Server
 
-class Server:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.host, self.port))
-        self.server_ui = None
+class ServerUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Server")
+
+        self.label = tk.Label(self.root, text="Server is running.")
+        self.label.pack()
+
+        self.quit_button = tk.Button(self.root, text="Quit", command=self.root.quit)
+        self.quit_button.pack()
+
+        self.server = Server("127.0.0.1", 8080)
 
     def start(self):
-        self.server_ui = ServerUI()  # Create an instance of ServerUI
-        self.server_ui.start()  # Start the Tkinter main loop in a separate thread
-        self.server_socket.listen(1)
-        print(self.host)
-        print('waiting for incoming connections...')
-
-        conn, addr = self.server_socket.accept()
-        print(addr, "has connected to the server")
-
-        filename = self.server_ui.get_filename()  # Get the filename from the UI
-        self.send_file(conn, filename)
-
-    def send_file(self, conn, filename):
-        file = open(filename, 'rb')
-        filedata = file.read(1024)
-        print(filedata)
-        conn.send(filedata)
-        print("Data has been transmitted successfully")
+        self.server.start()
+        self.root.mainloop()
 
 if __name__ == "__main__":
-    host = "127.0.0.1"
-    port = 8080
-
-    server = Server(host, port)
-    server.start()
+    server_ui = ServerUI()
+    server_ui.start()
